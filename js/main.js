369bec559070d30cae612874274df08def44f791
seungyoonLee <syoon624@naver.com>
2022-10-12 16:51:52 +0900

    파일 수정

--- js/main.js
+++ js/main.js
@@ -28,28 +28,51 @@ const Render = (() => {
 
 //좌표값 구하기
 let target = document.querySelector(".container1");
+let targetTop = target.getBoundingClientRect().top + window.scrollY - 100;
+
 let fixTarget = document.querySelector('#fix1');
 let marginTarget = document.querySelector('#margin1');
+let fixEndTarget = document.querySelector('#scroll-end1').getBoundingClientRect().top + window.scrollY - 150;
 
-let fixEndTarget = document.querySelector('#scroll-end1').getBoundingClientRect().bottom + window.scrollY;
-let targetTop = target.getBoundingClientRect().top + window.scrollY;
 
-let abTop = window.pageYOffset + target.getBoundingClientRect().top;
+let target2 = document.querySelector(".container3");
+let targetTop2 = target2.getBoundingClientRect().top + window.scrollY;
 
-let winY = window.pageYOffset;
+let fixTarget2 = document.querySelector('#fix2');
+let marginTarget2 = document.querySelector('#margin2');
+let fixEndTarget2 = document.querySelector('#scroll-end2').getBoundingClientRect().bottom + window.scrollY;
 
-console.log(targetTop)
-console.log(fixEndTarget)
+console.log(fixEndTarget2); // 2000
+console.log(targetTop2); //2200
 
+//스크롤 값에 따라 위치 선정
 window.addEventListener('scroll', () => {
-    if(window.scrollY >= fixEndTarget) {
+    if(window.scrollY >= 2100) {
+        console.log(3)
+        fixTarget2.classList.remove('fixed2')
+        target2.classList.add('flex-end')
+    } else if(window.scrollY <= 2100 && window.scrollY >= 1850) { // 좌표값 하드코딩 ㅠㅠ
+        console.log(4)
+        fixTarget2.classList.add('fixed2')
+        target2.classList.remove('flex-end')
+    } else if(window.scrollY >= fixEndTarget) {
+        console.log(1)
         fixTarget.classList.remove('fixed')
         marginTarget.classList.remove('margined')
+        fixTarget2.classList.remove('fixed2')
+        marginTarget2.classList.remove('margined')
+        target.classList.add('flex-end')
     } else if(window.scrollY >= targetTop) {
+        console.log(2)
         fixTarget.classList.add('fixed')
         marginTarget.classList.add('margined')
     } else {
+        console.log(5)
         fixTarget.classList.remove('fixed')
         marginTarget.classList.remove('margined')
+        target.classList.remove('flex-end')
+        fixTarget2.classList.remove('fixed2')
+        marginTarget2.classList.remove('margined')
+        target2.classList.remove('flex-end')
     }
 })
