306dfd009e0b1d4d014a73f0ba0e0179e335e078
seungyoonLee <syoon624@naver.com>
2022-10-14 19:23:02 +0900

    검색 창 구현 완료

--- js/main.js
+++ js/main.js
@@ -42,32 +42,30 @@ let fixTarget2 = document.querySelector('#fix2');
 let marginTarget2 = document.querySelector('#margin2');
 let fixEndTarget2 = document.querySelector('#scroll-end2').getBoundingClientRect().bottom + window.scrollY;
 
-console.log(fixEndTarget2); // 2000
-console.log(targetTop2); //2200
 
 //스크롤 값에 따라 위치 선정
 window.addEventListener('scroll', () => {
     if(window.scrollY >= 2100) {
-        console.log(3)
+        // console.log(3)
         fixTarget2.classList.remove('fixed2')
         target2.classList.add('flex-end')
     } else if(window.scrollY <= 2100 && window.scrollY >= 1850) { // 좌표값 하드코딩 ㅠㅠ
-        console.log(4)
+        // console.log(4)
         fixTarget2.classList.add('fixed2')
         target2.classList.remove('flex-end')
     } else if(window.scrollY >= fixEndTarget) {
-        console.log(1)
+        // console.log(1)
         fixTarget.classList.remove('fixed')
         marginTarget.classList.remove('margined')
         fixTarget2.classList.remove('fixed2')
         marginTarget2.classList.remove('margined')
         target.classList.add('flex-end')
     } else if(window.scrollY >= targetTop) {
-        console.log(2)
+        // console.log(2)
         fixTarget.classList.add('fixed')
         marginTarget.classList.add('margined')
     } else {
-        console.log(5)
+        // console.log(5)
         fixTarget.classList.remove('fixed')
         marginTarget.classList.remove('margined')
         target.classList.remove('flex-end')
@@ -76,3 +74,35 @@ window.addEventListener('scroll', () => {
         target2.classList.remove('flex-end')
     }
 })
+
+// 검색 창 구현
+const searchTarget = document.querySelector('.search-container')
+const headerTarget = document.querySelector('.header')
+const removeTarget = [ 
+        document.querySelector('.container1'),
+        document.querySelector('.container2'),
+        document.querySelector('.container3'),
+        document.querySelector('.container4'),
+        document.querySelector('.container5'),
+        document.querySelector('footer')
+]
+const mainTarget = document.querySelector('main')
+
+const OpenSearch = () => {
+    searchTarget.classList.remove('hidden')
+    headerTarget.classList.add('hidden')
+    removeTarget.forEach(arr => {
+        arr.classList.add('remove')
+    })
+    mainTarget.classList.add('do-opacity')
+}
+
+const CloseSearch = () => {
+    searchTarget.classList.add('hidden')
+    headerTarget.classList.remove('hidden')
+    removeTarget.forEach(arr => {
+        arr.classList.remove('remove')
+    })
+    mainTarget.classList.remove('do-opacity')
+    mainTarget.classList.add('do-not-opacity')
+}  
